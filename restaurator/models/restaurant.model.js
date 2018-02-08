ea1c3f72b204b0352c60a2dfd8ed36522482f991
Cristina Bertrand <cristina@11870.com>
2018-02-08 16:58:06 +0100

    feat: restaurants controller

--- restaurator/models/restaurant.model.js
+++ restaurator/models/restaurant.model.js
@@ -7,7 +7,6 @@ const restaurantSchema = new mongoose.Schema({
          required: [true, 'Name is required'],
          unique: true,
      },
-
      email: {
          type: mongoose.SchemaTypes.Email,
          required: [true, 'Email is required'],
@@ -16,27 +15,20 @@ const restaurantSchema = new mongoose.Schema({
          type: String,
          required: [true, 'User needs a password']
      },
-
      category: {
          type: String,
-
      },
-
      description: {
          type: String,
-
      },
-
      location: {
-         type: String,
-
-     },
-
+        type: { type: String },
+        coordinates: [Number]
+      },
      imageUrl: {
          type: String,
-
      },
-
+     file: String
 
  }, { timestamps: true });
 
