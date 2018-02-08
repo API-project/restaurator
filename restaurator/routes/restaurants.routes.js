e74822921e7c775963b77ab1747ad8a191f0d0be
Cristina Bertrand <cristina@11870.com>
2018-02-08 17:03:05 +0100

    fix: restaurant route

--- restaurator/routes/restaurants.routes.js
+++ restaurator/routes/restaurants.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const restaurantsController = require('../controllers/restaurants.controller');
 
-
+router.get('/index', restaurantsController.index);
+router.get('/new', restaurantsController.new);
+router.post('/', restaurantsController.create);
+router.post('/:id/delete', restaurantsController.delete);
+router.get('/:id/pic', restaurantsController.pic);
 
 module.exports = router;
