291b37d6798913491ffcb2e6ff054f9d9c158372
Cristina Bertrand <cristina@11870.com>
2018-02-24 13:05:51 +0100

    feat:all the restaurant models created

--- restaurator/models/eltenedor.model.js
+++ restaurator/models/eltenedor.model.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
 
-const ElTenedorEatSchema = new mongoose.Schema({
+const ElTenedorSchema = new mongoose.Schema({
      name: {
          type: String,
      },
-     
-);
+     rating: {
+         type: Number,
+     },
+     geo_location: {
+       lat: {type: Number},
+       lng: {type: Number}
+     },
+     location: {
+         type: String,
+     },
+     address: {
+         type: String,
+     },
+     id: {
+         type: String,
+     },
+     categories: [
+       {
+         type: String,
+     },]
+});
 
 const ElTenedor = mongoose.model('ElTenedor', ElTenedorSchema);
 module.exports = ElTenedor;
